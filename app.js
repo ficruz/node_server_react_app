f86343e5c8984d320a59326847ff6de6e4f9fb97
ficruz <ficruz@users.noreply.github.com>
2021-06-24 22:10:41 -0400

    change the okta login/callback to callback

--- app.js
+++ app.js
@@ -3,18 +3,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 80;
+const port = 5000;
 
 app.use(express.static(path.join(__dirname, "build")));
 
-if (process.env.NODE_ENV === "production") {
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-}
-
-app.get("/", (req, res) => {
-  res.send("hola");
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
